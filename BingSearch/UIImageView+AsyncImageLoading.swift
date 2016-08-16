2b7e9cc107555cfca3013e264f252834a4e2266b
Sergey Nesteruk <dj_decibell@mail.ru>
2016-08-16 12:40:47 +0300

    Removed handling of spinner on images

--- BingSearch/UIImageView+AsyncImageLoading.swift
+++ BingSearch/UIImageView+AsyncImageLoading.swift
@@ -13,18 +13,7 @@ extension UIImageView
 {
     func loadImageFromURL(url: NSURL, completion: (()->())? = nil)
     {
-//        let placeholderImage = UIImage(named: "preview_icon")
-        let placeholderImage: UIImage? = nil
-//        let spinnerTag = 1001
-//        if viewWithTag(spinnerTag) == nil
-//        {
-//            let spinner = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
-//            spinner.tag = spinnerTag
-//            addSubview(spinner)
-//            spinner.center = CGPoint(x: bounds.width / 2, y: bounds.height / 2)
-//            spinner.startAnimating()
-//        }
-//        image = placeholderImage
+        let placeholderImage = UIImage(named: "preview_icon")
         let timestamp = NSDate.timeIntervalSinceReferenceDate()
         RequestTimeStampsManager.sharedInstance[self] = timestamp
         let currentSessionTask = NSURLSession.sharedSession().dataTaskWithURL(url) {[weak self](data, response, error) in
@@ -35,10 +24,6 @@ extension UIImageView
                 {
                     dispatch_async(dispatch_get_main_queue()) {
                         strongSelf.image = placeholderImage
-//                        if let spinner = strongSelf.viewWithTag(spinnerTag)
-//                        {
-//                            spinner.removeFromSuperview()
-//                        }
                         completion?()
                     }
                 }
@@ -51,10 +36,6 @@ extension UIImageView
                 {
                     dispatch_async(dispatch_get_main_queue()) {
                         strongSelf.image = placeholderImage
-//                        if let spinner = strongSelf.viewWithTag(spinnerTag)
-//                        {
-//                            spinner.removeFromSuperview()
-//                        }
                         completion?()
                     }
                 }
@@ -67,10 +48,6 @@ extension UIImageView
                 {
                     dispatch_async(dispatch_get_main_queue()) {
                         strongSelf.image = image
-//                        if let spinner = strongSelf.viewWithTag(spinnerTag)
-//                        {
-//                            spinner.removeFromSuperview()
-//                        }
                         completion?()
                     }
                 }
@@ -81,10 +58,6 @@ extension UIImageView
                 {
                     dispatch_async(dispatch_get_main_queue()) {
                         strongSelf.image = placeholderImage
-//                        if let spinner = strongSelf.viewWithTag(spinnerTag)
-//                        {
-//                            spinner.removeFromSuperview()
-//                        }
                         completion?()
                     }
                 }
